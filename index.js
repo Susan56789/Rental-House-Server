a16407bd52ae1f74f8d153e329b22d016915e80c
Susan Wairimu <mwihaki2susan@gmail.com>
2022-01-26 19:14:16 +0300

    mapped through data for post request

--- index.js
+++ index.js
@@ -117,78 +117,85 @@ app.post("/user/login", (req, res) => {
   const user = req.body.username;
   const pwd = req.body.password;
 
-  let userData = users(req.headers);
-  // return 400 status if username/password is not exist
-  if (!user || !pwd) {
-    return res.status(400).json({
-      error: true,
-      message: "Username or Password is required.",
-    });
-  }
-  // return 401 status if the credential is not match.
-  if (user !== userData.username || pwd !== userData.password) {
-    return res.status(401).json({
-      error: true,
-      message: "Username or Password is wrong.",
-    });
-  }
-  // generate token
-  const token = utils.generateToken(userData);
-  // get basic user details
-  const userObj = utils.getCleanUser(userData);
-  // return the token along with user details
-  return res.json({ user: userObj, token });
-});
+  const Data = users();
 
-// verify the token and return it if it's valid
-app.get("/verifyToken", function (req, res) {
-  // check header or url parameters or post parameters for token
-  var token = req.query.token;
-  if (!token) {
-    return res.status(400).json({
-      error: true,
-      message: "Token is required.",
-    });
-  }
-  // check token that was passed by decoding token using secret
-  jwt.verify(token, process.env.JWT_SECRET, function (err, user) {
-    if (err)
-      return res.status(401).json({
-        error: true,
-        message: "Invalid token.",
-      });
+  console.log(Data);
 
-    // return 401 status if the userId does not match.
-    if (user.userId !== userData.id) {
-      return res.status(401).json({
+  Data.map((userData) => {
+    // return 400 status if username/password is not exist
+    if (!user || !pwd) {
+      return res.status(400).json({
         error: true,
-        message: "Invalid user.",
+        message: "Username or Password is required.",
       });
     }
+    // return 401 status if the credential is not match.
+    if (user !== userData.username || pwd !== userData.password) {
+      console.log(user);
+      console.log(userData.username);
+      return res.status(401).json({
+        error: true,
+        message: "Username or Password is wrong.",
+      });
+    }
+    // generate token
+    const token = utils.generateToken(userData);
     // get basic user details
-    var userObj = utils.getCleanUser(userData);
+    const userObj = utils.getCleanUser(userData);
+    // return the token along with user details
     return res.json({ user: userObj, token });
   });
-});
 
-//middleware that checks if JWT token exists and verifies it if it does exist.
-//In all future routes, this helps to know if the request is authenticated or not.
-app.use(function (req, res, next) {
-  // check header or url parameters or post parameters for token
-  var token = req.headers["authorization"];
-  if (!token) return next(); //if no token, continue
-
-  token = token.replace("Bearer ", "");
-  jwt.verify(token, process.env.JWT_SECRET, function (err, user) {
-    if (err) {
-      return res.status(401).json({
+  // verify the token and return it if it's valid
+  app.get("/verifyToken", function (req, res) {
+    // check header or url parameters or post parameters for token
+    var token = req.query.token;
+    if (!token) {
+      return res.status(400).json({
         error: true,
-        message: "Invalid user.",
+        message: "Token is required.",
       });
-    } else {
-      req.user = user; //set the user to req so other routes can use it
-      next();
     }
+    // check token that was passed by decoding token using secret
+    jwt.verify(token, process.env.JWT_SECRET, function (err, user) {
+      if (err)
+        return res.status(401).json({
+          error: true,
+          message: "Invalid token.",
+        });
+
+      // return 401 status if the userId does not match.
+      if (user.userId !== userData.id) {
+        return res.status(401).json({
+          error: true,
+          message: "Invalid user.",
+        });
+      }
+      // get basic user details
+      var userObj = utils.getCleanUser(userData);
+      return res.json({ user: userObj, token });
+    });
+  });
+
+  //middleware that checks if JWT token exists and verifies it if it does exist.
+  //In all future routes, this helps to know if the request is authenticated or not.
+  app.use(function (req, res, next) {
+    // check header or url parameters or post parameters for token
+    var token = req.headers["authorization"];
+    if (!token) return next(); //if no token, continue
+
+    token = token.replace("Bearer ", "");
+    jwt.verify(token, process.env.JWT_SECRET, function (err, user) {
+      if (err) {
+        return res.status(401).json({
+          error: true,
+          message: "Invalid user.",
+        });
+      } else {
+        req.user = user; //set the user to req so other routes can use it
+        next();
+      }
+    });
   });
 });
 
