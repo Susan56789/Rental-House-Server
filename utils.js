55a346bdb065260c30018ac88953f0fb01e4452f
Susan Wairimu <mwihaki2susan@gmail.com>
2022-01-27 22:52:52 +0300

    updates

--- utils.js
+++ utils.js
@@ -16,7 +16,7 @@ function generateToken(user) {
   };
 
   return jwt.sign(u, process.env.JWT_SECRET, {
-    expiresIn: "600s", // expires in 24 hours
+    expiresIn: 60 * 60 * 72, // expires in 72 hours
   });
 }
 
